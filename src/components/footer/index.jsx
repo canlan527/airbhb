d3ea50cc24ce40ae6a84ade78ed874409d3e5656
灿烂 <1481086138@qq.com>
2023-03-09 17:24:54 +0800

    icon属性修改和新增footer布局

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo } from "react";
+import { FooterWrapper } from "./style";
+import Data from "@/assets/data/footer.json";
 
 const Footer = memo(() => {
   return (
-    <div>Footer</div>
-  )
-})
+    <FooterWrapper>
+      <div className="content">
+        {Data.map((item) => (
+          <div className="list" key={item.name}>
+            <h5 className="title">{item.name}</h5>
+            {item.list.map((innerItem) => ( 
+              <li key={innerItem} className="item">{innerItem}</li>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="bottom">
+        © 2023 Airbnb, Inc. All rights reserved. 条款 · 隐私政策 · 网站地图 · 全国旅游投诉渠道 12301
+      </div>
+    </FooterWrapper>
+  );
+});
 
-export default Footer
+export default Footer;
