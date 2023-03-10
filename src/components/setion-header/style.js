0ffc643d0324c2545996da95c42667fda957f4ea
灿烂 <1481086138@qq.com>
2023-03-10 11:36:55 +0800

    修改section-header样式

--- src/components/setion-header/style.js
+++ src/components/setion-header/style.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const HeaderWrapper = styled.div`
   padding: 16px 0 0;
   .title {
-    font-size: 28px;
+    font-size: 26px;
+    font-weight: bold;
   }
   .subTitle {
     color: #424242;
