ec48b0fe44d3ff1e06dd89f88e961d30771f673f
灿烂 <1481086138@qq.com>
2023-03-10 13:57:43 +0800

    获取home-discount数据

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,31 +1,46 @@
-import {getHomeGoodPriceData, getHomeHighScoreData} from '@/services' 
+import {
+  getHomeGoodPriceData,
+  getHomeHighScoreData,
+  getHomeDiscountData,
+} from "@/services";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchHomeDataAction = createAsyncThunk('fetch/homeData',  (payload, {dispatch, getState}) => {
-  getHomeGoodPriceData().then(res => {
-    dispatch(changeGoodPriceInfoAction(res))
-  })
-  getHomeHighScoreData().then(res => {
-    dispatch(changeHighScoreInfo(res))
-  })
-})
+export const fetchHomeDataAction = createAsyncThunk(
+  "fetch/homeData",
+  (payload, { dispatch, getState }) => {
+    getHomeGoodPriceData().then((res) => {
+      dispatch(changeGoodPriceInfoAction(res));
+    });
+    getHomeHighScoreData().then((res) => {
+      dispatch(changeHighScoreInfo(res));
+    });
+    getHomeDiscountData().then((res) => {
+      dispatch(changeDiscountAction(res));
+    });
+  }
+);
 
 const homeSlice = createSlice({
   name: "home",
   initialState: {
     goodPriceInfo: {}, //高性价比数据 包含list和title
     highScoreInfo: {}, // 高评分 包含list和title
+    discountInfo: {}, // 折扣数据
   },
   reducers: {
     changeGoodPriceInfoAction(state, { payload }) {
       state.goodPriceInfo = payload;
     },
-    changeHighScoreInfo(state, {payload}) {
-      state.highScoreInfo = payload
-    }
+    changeHighScoreInfo(state, { payload }) {
+      state.highScoreInfo = payload;
+    },
+    changeDiscountAction(state, { payload }) {
+      state.discountInfo = payload;
+    },
   },
 });
 
-export const { changeGoodPriceInfoAction, changeHighScoreInfo, } = homeSlice.actions;
+export const { changeGoodPriceInfoAction, changeHighScoreInfo, changeDiscountAction } =
+  homeSlice.actions;
 
 export default homeSlice.reducer;
