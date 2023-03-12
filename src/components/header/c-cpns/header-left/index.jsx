17ba1c628e3b976c4f120c78096349bcbae48f9d
灿烂 <1481086138@qq.com>
2023-03-12 21:42:41 +0800

    header部分logo跳转首页

--- src/components/header/c-cpns/header-left/index.jsx
+++ src/components/header/c-cpns/header-left/index.jsx
@@ -1,13 +1,19 @@
 import React, { memo } from 'react'
-
+import { useNavigate } from 'react-router-dom'
 import IconLogo from '@/assets/svg/icon_logo'
 import TextLogo from '@/assets/img/home/logo-text.jpeg'
 import { LeftWrapper } from './styled'
 
 const HeaderLeft = memo(() => {
+
+  const navigate = useNavigate()
+  function handleClick() {
+    navigate('/')
+  }
+
   return (
     <LeftWrapper>
-      <div className="logo">
+      <div className="logo" onClick={handleClick}>
         <IconLogo />
       </div>
       <div className="text-logo">
