995e2654d08e2b957def6f73fa7df8ec25065174
灿烂 <1481086138@qq.com>
2023-03-19 21:17:30 +0800

    完成图片预览功能

--- src/base-ui/picture-preview/style.js
+++ src/base-ui/picture-preview/style.js
@@ -62,14 +62,67 @@ export const PreviewWrapper = styled.div`
     }
   }
   .preview-indicator {
+    position: relative;
     width: 100%;
     height: 200px;
-    background: tan;
     margin: 20px 0 40px;
+    .toolbar {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 1084px;
+      display: flex;
+      justify-content: space-between;
+      color: #fff;
+      .toolbar-btn {
+        cursor: pointer;
+      }
+    }
+    .preview-list {
+      position: absolute;
+      top: 20px;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 1084px;
+      margin-top: 12px;
+      overflow: hidden;
+      .preview-item {
+        margin-right: 12px;
+        width: 100px;
+        height: ${props => props.indicatorHeight}px;
+        position: relative;
+        transition: all 200ms ease;
+        img {
+          width: 100%;
+          height: 100%;
+          cursor: pointer;
+        }
+        .preview-cover {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, 0.5);
+          opacity: 1;
+          cursor: pointer;
+          &.active {
+            opacity: 0;
+          }
+        }
+
+        &:last-of-type {
+          margin-right: 0;
+        }
+      }
+    }
   }
 
   .fade-enter {
-    opacity: .7;
+    opacity: 0.7;
     transform: translateX(
       ${(props) => (props.direction === "next" ? "50%" : "-50%")}
     );
@@ -83,8 +136,7 @@ export const PreviewWrapper = styled.div`
     opacity: 1;
   }
   .fade-exit-active {
-    opacity: .7;
+    opacity: 0.7;
     transition: all 150ms;
-
   }
 `;
--- src/views/detail/c-cpns/detail-pictures/index.jsx
+++ src/views/detail/c-cpns/detail-pictures/index.jsx
@@ -1,16 +1,16 @@
 import PropTypes from "prop-types";
-import React, { memo,useState } from "react";
+import React, { memo, useState } from "react";
 import { useSelector } from "react-redux";
 import PicturePreview from "@/base-ui/picture-preview";
 import { DetailPictureWrapper } from "./style";
 const DetailPicture = memo((props) => {
-  const [showPreview, setShowPreview] = useState(false)
+  const [showPreview, setShowPreview] = useState(false);
   const { info } = useSelector((state) => ({
     info: state.detail.detailInfo,
   }));
 
   function handlePreview() {
-    setShowPreview(!showPreview)
+    setShowPreview(!showPreview);
   }
 
   return (
@@ -23,7 +23,11 @@ const DetailPicture = memo((props) => {
       </div>
       <div className="picture-right">
         {info?.picture_urls?.slice(1, 5).map((item) => (
-          <div className="div-pic-item right" key={item} onClick={handlePreview}>
+          <div
+            className="div-pic-item right"
+            key={item}
+            onClick={handlePreview}
+          >
             <img src={item} alt="" />
             <div className="detail-pic-cover"></div>
           </div>
@@ -32,7 +36,13 @@ const DetailPicture = memo((props) => {
       <div className="operation-btn" onClick={handlePreview}>
         查看照片
       </div>
-      {showPreview && <PicturePreview showPreview={handlePreview} pictures={info.picture_urls} />}
+      {showPreview && (
+        <PicturePreview
+          showPreview={handlePreview}
+          pictures={info.picture_urls}
+          name={info.name}
+        />
+      )}
     </DetailPictureWrapper>
   );
 });
