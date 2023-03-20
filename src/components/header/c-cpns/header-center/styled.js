69d0e948409d8ac3932a2b7de76f44b6dc575e34
灿烂 <1481086138@qq.com>
2023-03-20 19:57:32 +0800

    header动画切换

--- src/components/header/c-cpns/header-center/styled.js
+++ src/components/header/c-cpns/header-center/styled.js
@@ -1,23 +1,29 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const CenterWrapper = styled.div`
-  @media screen and (min-width: 350px) and (max-width: 1024px){
-      display:none;
-    } 
+  position: relative;
+  display: flex;
+  justify-content: center;
+  height: 48px;
+  @media screen and (min-width: 350px) and (max-width: 1024px) {
+    display: none;
+  }
+
   .search-bar {
+    position: absolute;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    width:300px;
-    height:48px;
+    width: 300px;
+    height: 48px;
     box-sizing: border-box;
     padding: 0 8px;
     border: 1px solid #ddd;
     border-radius: 24px;
     cursor: pointer;
 
-    ${props => props.theme.mixin.boxShadow}
-    
+    ${(props) => props.theme.mixin.boxShadow}
+
     .text {
       padding: 0 16px;
       color: #222;
@@ -30,7 +36,7 @@ export const CenterWrapper = styled.div`
       justify-content: center;
       width: 32px;
       height: 32px;
-      background: ${props => props.theme.color.primaryColor};
+      background: ${(props) => props.theme.color.primaryColor};
       color: #fff;
       border-radius: 50%;
     }
@@ -46,10 +52,46 @@ export const CenterWrapper = styled.div`
       top: 100%;
       left: 50%;
       transform: translateX(-50%);
-      /* width: 100%; */
-      /* display: flex;
-      align-items:center;
-      justify-content: center; */
     }
+  }
+
+  .bar-enter {
+    transform: scale(2.85714, 1.375) translateY(58px);
+    opacity: 0;
+  }
+
+  .bar-enter-active {
+    transition: all 250ms ease;
+    transform: scale(1) translateY(0);
+    opacity: 1;
+  }
+
+  .bar-exit {
+    opacity: 0;
+  }
+  .bar-exit-active {
+    opacity: 0;
+  }
+
+  .detail-exit {
+    transform: scale(1) translateY(0);
+    opacity: 0;
+  }
+
+  .detail-exit-active {
+    transition: all 250ms ease;
+    transform: scale(0.35, 0.727273) translateY(-58px);
+    opacity: 0;
+  }
+
+  .detail-enter {
+    transform: scale(0.35, 0.727273) translateY(-58px);
+    opacity: 0;
+  }
+
+  .detail-enter-active {
+    transform: scale(1) translateY(0);
+    opacity: 1;
+    transition: all 250ms ease;
   }
 `;
